94dfcb65877c480a0116bbbe75257afff570248d
Vinicius <58366766+viniciusac92@users.noreply.github.com>
2025-03-20 15:26:03 -0300

    refac: ajustes nos espaçamentos

--- src/containers/LoginContainer.tsx
+++ src/containers/LoginContainer.tsx
@@ -14,7 +14,7 @@ interface LoginContainerProps {
 
 const LoginContainer: React.FC<LoginContainerProps> = ({ form, onSubmit }) => {
     return (
-        <div className="flex flex-row justify-end px-6 sm:px-20 pt-40">
+        <div className="flex flex-row justify-end px-4 sm:px-20 pt-40">
             <IntroSection />
             <div className="flex flex-col items-start max-w-99.5 w-full">
                 <h2 className="flex flex-start text-xl font-bold mb-4 text-white">Entrar</h2>
